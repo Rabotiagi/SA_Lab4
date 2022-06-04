4e17ef9c2789e587fdf7f055885afdb0455c752a
yaroslav_els <yarter003@gmail.com>
2022-06-04 23:28:17 +0300

    parser function implemented

--- main.go
+++ main.go
@@ -2,16 +2,44 @@ package main
 
 import (
 	"eventloop/engine"
+	"strconv"
+	"strings"
 )
+
+func parse(commandLine string) engine.Command {
+	parts := strings.Fields(commandLine)
+
+	switch parts[0] {
+	case "print":
+		return engine.PrintCommand(parts[1])
+	case "add":
+		num1, err := strconv.Atoi(parts[1])
+		num2, err := strconv.Atoi(parts[2])
+		if err != nil {
+			return engine.PrintCommand("SYNTAX ERROR: " + err.Error())
+		}
+		return engine.AddCommand{A: num1, B: num2}
+	}
+
+	return engine.PrintCommand("SYNTAX ERROR: Unknown instruction")
+}
 
 func main() {
 	loop := new(engine.Loop)
-
 	loop.Start()
 
-	loop.Post(engine.PrintCommand("hello"))
-	loop.Post(&engine.AddCommand{A: 4, B: 5})
-	loop.Post(engine.PrintCommand("hello2"))
+	// if input, err := os.Open("test.txt"); err == nil {
+	// 	defer input.Close()
+	// 	scanner := bufio.NewScanner(input)
+	// 	for scanner.Scan() {
+	// 		commandLine := scanner.Text()
+	// 		cmd := parse(commandLine)
+	// 		loop.Post(cmd)
+	// 	}
+	// }
+
+	cmd := parse("add 1 9")
+	loop.Post(cmd)
 
 	loop.AwaitFinish()
 }
